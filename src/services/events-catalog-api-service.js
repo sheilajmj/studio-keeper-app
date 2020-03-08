d3714e53ae0ccb4dc0879761f085c5b686d279bd
starbone Jagla <starbone@gmail.com>
2020-03-08 13:14:50 -0600

    troubleshooting

--- src/services/events-catalog-api-service.js
+++ src/services/events-catalog-api-service.js
@@ -7,12 +7,13 @@ const EventsCatalogService = {
                 'authorization': 'bearer db943962-4003-4d18-ab25-9f0c6bb2679c'
                 //`bearer ${TokenService.getAuthToken()}`,
             },
-            body: JSON.stringify({
+            query: JSON.stringify({
                 key: key,
                 value: value,
         })
     })
         .then(res => 
+            console.log(res.ok, "here is res OK?")
             (!res.ok)
             ?res.json().then(e => Promise.reject(e))
             : res.json()
